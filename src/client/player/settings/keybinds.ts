4681ee01b7abb6771c418793ff80c920fb61ddec
Murmanox <murmanox@hotmail.com>
2021-06-06 15:20:27 +0100

    added movement category and reset key

--- src/client/player/settings/keybinds.ts
+++ src/client/player/settings/keybinds.ts
@@ -1,8 +1,11 @@
 const keybinds = {
-	left: [Enum.KeyCode.S] as Enum.KeyCode[],
-	right: [Enum.KeyCode.F] as Enum.KeyCode[],
-	up: [Enum.KeyCode.E] as Enum.KeyCode[],
-	down: [Enum.KeyCode.D] as Enum.KeyCode[],
+	movement: {
+		left: [Enum.KeyCode.S] as Enum.KeyCode[],
+		right: [Enum.KeyCode.F] as Enum.KeyCode[],
+		up: [Enum.KeyCode.E] as Enum.KeyCode[],
+		down: [Enum.KeyCode.D] as Enum.KeyCode[],
+	},
+	reset: [Enum.KeyCode.R] as Enum.KeyCode[],
 }
 
 export const settings = {
