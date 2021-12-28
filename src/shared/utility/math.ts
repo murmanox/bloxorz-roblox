ea8277cf0dfc09bf74dfc16eb678c38505dd2148
Murmanox <murmanox@hotmail.com>
2021-12-28 23:15:20 +0000

    added random method

--- src/shared/utility/math.ts
+++ src/shared/utility/math.ts
@@ -1,7 +1,23 @@
+const random_obj = new Random()
+
 export namespace Math {
 	export const QUARTER_PI = math.pi / 4
 	export const HALF_PI = math.pi / 2
 	export const TWO_PI = 2 * math.pi
+
+	export function random(): number
+	export function random(max: number): number
+	export function random(min: number, max: number): number
+	export function random(min?: number, max?: number): number {
+		if (typeIs(min, "number")) {
+			if (typeIs(max, "number")) {
+				return random_obj.NextNumber(min, max)
+			} else {
+				return random_obj.NextNumber(0, min)
+			}
+		}
+		return random_obj.NextNumber()
+	}
 
 	export function roundTo(n: number, decimals: number = 0) {
 		const mult = 10 ** decimals
