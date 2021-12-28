7d2926b3dd605d41f06e6fa16a098aa8c341d0d9
Murmanox <murmanox@hotmail.com>
2021-12-28 23:11:50 +0000

    added utility types

--- types/utility-types.d.ts
+++ types/utility-types.d.ts
@@ -1,3 +1,11 @@
 declare type PickOne<T> = {
 	[K in keyof T]: Record<K, T[K]> & Partial<Record<Exclude<keyof T, K>, undefined>>
 }[keyof T]
+
+interface BoardPosition {
+	row: number
+	column: number
+}
+
+declare type VoidPromise = Promise<void>
+declare type VoidCallback<T extends Array<unknown>> = (...args: T) => void
