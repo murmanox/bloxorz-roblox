44a77e4d9b854f510cc51f0b9cfb5a050cc27ff3
Murmanox <murmanox@hotmail.com>
2021-12-28 23:12:32 +0000

    added default debug part size

--- src/shared/utility/debug.ts
+++ src/shared/utility/debug.ts
@@ -1,7 +1,11 @@
 import Make from "@rbxts/make"
 import { Workspace } from "@rbxts/services"
 
-export function makeDebugPart(position: Vector3, size: Vector3, parent: Instance = Workspace) {
+export function makeDebugPart(
+	position: Vector3,
+	size = new Vector3(0.1, 0.1, 0.1),
+	parent: Instance = Workspace
+) {
 	return Make("Part", {
 		Parent: parent,
 		Anchored: true,
