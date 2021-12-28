01ae6a2685d2c23dd4fdae448fe6322b1f95ba41
Murmanox <murmanox@hotmail.com>
2021-12-28 23:17:05 +0000

    added tile animation functions

---
+++ src/client/player/game/effects/tile-animations.ts
@@ -0,0 +1,49 @@
+import { loli } from "@rbxts/loli-tween-animator"
+import { tile_config } from "shared/game/tiles/tile-config"
+import { Math } from "shared/utility/math"
+import { ITile } from "types/interfaces/tile-types"
+
+export function verticalInAnimation(tile: ITile, position: Vector3): Promise<void> {
+	const instance = tile.instance
+	if (!instance) return Promise.resolve(undefined)
+
+	const goal = new CFrame(position, position.add(instance.GetPivot().LookVector))
+	instance.PivotTo(goal.add(new Vector3(0, -15, 0)))
+
+	const config = tile_config
+	const delay = Math.random(config.tween_delay.min, config.tween_delay.max)
+	return loli
+		.to(instance.GetPivot(), {
+			duration: 0.5,
+			delay: delay,
+			goal: goal,
+			ease: "power3.out",
+			onUpdate: (cframe) => {
+				instance.PivotTo(cframe)
+			},
+		})
+		.then()
+}
+
+export function verticalOutAnimation(tile: ITile): Promise<void> {
+	const instance = tile.instance
+	if (!instance) return Promise.resolve(undefined)
+
+	const change = new Vector3(0, -13, 0)
+	const tile_cframe = instance.GetPivot()
+	const goal = tile_cframe.add(change)
+
+	const config = tile_config
+	const delay = Math.random(config.tween_delay.min, config.tween_delay.max)
+	return loli
+		.to(tile_cframe, {
+			duration: 0.5,
+			delay,
+			goal,
+			ease: "power2.in",
+			onUpdate: (cframe) => {
+				instance.PivotTo(cframe)
+			},
+		})
+		.then()
+}
