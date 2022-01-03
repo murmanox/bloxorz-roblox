70fbe563e2221e02c238620a0b9cd25b5443857d
Murmanox <murmanox@hotmail.com>
2022-01-03 17:23:41 +0000

    moved check logic into board-logic

--- src/client/player/game/board/board.ts
+++ src/client/player/game/board/board.ts
@@ -26,7 +26,6 @@ export class Board {
 	static EMPTY = BoardEnums.EMPTY
 
 	private state: BoardState = BoardState.Unloaded
-	public previous_checks = new Map<Block, Vector3[]>()
 
 	// class
 	private board_logic = new BoardLogic()
@@ -49,7 +48,8 @@ export class Board {
 		this.loader.onUnload(setState(BoardState.Unloading))
 		this.loader.onUnloaded(setState(BoardState.Unloaded))
 
-		// this.loader.onUnload(() => this.block_controller.)
+		this.board_logic.onLose(() => print("lose"))
+		this.board_logic.onWin(() => print("win"))
 	}
 
 	private setState(state: BoardState) {
@@ -65,65 +65,9 @@ export class Board {
 	}
 
 	// check all game logic to see if the player has won or lost
-	public check(current_checks = new Map<Block, Vector3[]>()) {
-		// Check if player has lost
-		const lose = this.board_logic.checkLoss(this.block_controller.blocks, this.tiles)
-		if (lose) {
-			print("lose")
-			return
-		}
-
-		// Check if player has won
-		const win = this.board_logic.checkWin(this.block_controller.blocks, this.tiles)
-		if (win) {
-			print("win")
-			return
-		}
-
-		// check combine
-		this.block_controller.checkCombine()
-
-		// run effects
-		const promises: Promise<boolean>[] = []
-		this.block_controller.blocks.forEach((block) => {
-			const m = this.previous_checks.get(block)
-			const n = current_checks.get(block)
-			const tiles = block.getPositions().map((p) => this.getTile(p.X, p.Z))
-
-			// Make sure blocks that were processed on previous checks don't get checked again.
-			// This prevents buttons from being activated repeatedly when a different block moves.
-			if (
-				(m && m.every((pos) => block.getPositions().includes(pos))) ||
-				(n && n.every((pos) => block.getPositions().includes(pos)))
-			) {
-				current_checks.set(block, block.getPositions())
-				return
-			}
-
-			current_checks.set(block, block.getPositions())
-			tiles.forEach((tile) => {
-				return promises.push(tile.stepped(block))
-			})
-		})
-
-		// Wait for all animations to finish
-		Promise.all(promises)
-			// Return a boolean that's true if the board needs to be checked again
-			.then((res) => (res as boolean[]).some((v) => v))
-			.then((check_again) => {
-				// Check again if stepping on a tile has changed the board in some way. Such as toggling a tile or teleporting a block.
-				if (check_again) {
-					task.wait()
-					this.check(current_checks)
-				} else {
-					this.previous_checks = current_checks
-				}
-			})
+	public check() {
+		this.board_logic.check(this, this.block_controller, this.tiles)
 	}
-
-	public checkCombine() {}
-
-	public runEffects() {}
 
 	public resetBoard() {
 		this.loader.reload()
