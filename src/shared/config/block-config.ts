915ddfe2f2597076afd1c1856665294797473ee0
Murmanox <murmanox@hotmail.com>
2022-01-03 17:24:25 +0000

    added debug and parent properties to block config

--- src/shared/config/block-config.ts
+++ src/shared/config/block-config.ts
@@ -1,4 +1,8 @@
+import { Workspace } from "@rbxts/services"
+
 export default {
+	debug: true,
+	parent: Workspace,
 	rotation_speed: math.rad(420),
 	spawn_height: new Vector3(0, 10, 0),
 	spawn_time: 0.4,
