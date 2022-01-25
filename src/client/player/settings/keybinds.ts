65719964e543d563bc0d21232acd4bf99064ddbc
Murmanox <murmanox@hotmail.com>
2022-01-25 01:58:57 +0000

    converted keybinds to Dynamic so they can be rebound

--- src/client/player/settings/keybinds.ts
+++ src/client/player/settings/keybinds.ts
@@ -1,15 +1,19 @@
-import { Action } from "@rbxts/gamejoy/out/Actions"
+import { Action, Dynamic, Sequence } from "@rbxts/gamejoy/out/Actions"
 
 export const settings = {
 	/** List of all input actions */
 	gamejoy: {
 		movement: {
-			left: new Action("S"),
-			right: new Action("F"),
-			up: new Action("E"),
-			down: new Action("D"),
+			left: new Dynamic("S"),
+			right: new Dynamic("F"),
+			up: new Dynamic("E"),
+			down: new Dynamic("D"),
 		},
-		reset: new Action("R"),
-		swap: new Action("Space"),
+		reset: new Dynamic("R"),
+		swap: new Dynamic("Space"),
+		debug: {
+			state: new Dynamic(new Sequence(["LeftShift", "D"])),
+			keys: new Dynamic(new Sequence(["LeftShift", "C"])),
+		},
 	},
 }
