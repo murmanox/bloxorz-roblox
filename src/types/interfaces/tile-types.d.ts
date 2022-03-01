fbca82de608d2b98ac07e85c64446ea9d8c5a3fc
Murmanox <murmanox@hotmail.com>
2022-03-01 22:08:17 +0000

    documented ITile interface

--- src/types/interfaces/tile-types.d.ts
+++ src/types/interfaces/tile-types.d.ts
@@ -30,12 +30,36 @@ interface EndProps {
 
 export type TileProperties = TileProps | ButtonProps | EndProps
 
+/**
+ * Interface that represents a tile on the Board
+ */
 export interface ITile {
+	/**
+	 * The model to use when displaying the tile
+	 */
 	instance?: Model
 
-	activate(action: ButtonProps["action"]): void
+	/**
+	 * Called when the tile is activated through external means, such as a player stepping on a button.
+	 * @param action The type of action to take on the tile
+	 */
+	activate(action: TileAction): void
+
+	/**
+	 * Called when the player moves above the tile.
+	 * @param block The block that's above the tile
+	 */
 	stepped(block: IBlock): Promise<boolean>
-	toggle(value: boolean): void
+
+	/**
+	 * If the tile is a losing position with a given block
+	 * @param block The block that's above the tile
+	 */
 	isLosingPosition(block: IBlock): boolean
+
+	/**
+	 * If the tile is a winning position with a given block
+	 * @param block The block that's above the tile
+	 */
 	isWinningPosition(block: IBlock): boolean
 }
